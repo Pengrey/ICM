080cc1b935332e5dd54d7100380fb554195b9ba1
Pengrey <rodrigoflima@ua.pt>
2022-03-16 09:05:14 +0000

    cleanup

--- coin/lib/main.dart
+++ coin/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 import 'money_data.dart';
+import 'transactions_data.dart';
 
 void main() => runApp(const MyApp());
 
@@ -163,116 +164,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   ];
 
   static get series {
-    final List<transactions> data = [
-      transactions(
-        date: 1,
-        money: 45,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 2,
-        money: 3,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 3,
-        money: 4,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 4,
-        money: 3,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 5,
-        money: 10,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 6,
-        money: 3,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 7,
-        money: 11,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 8,
-        money: 3,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 9,
-        money: 30,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 10,
-        money: 1,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 11,
-        money: 3,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 12,
-        money: 0,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 13,
-        money: 6,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 14,
-        money: 2,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 15,
-        money: 10,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 16,
-        money: 0,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 17,
-        money: 9,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-      transactions(
-        date: 18,
-        money: 4,
-        barColor:
-            charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
-      ),
-    ];
+    final List<transactions> data = TransactionsData.getData;
 
     List<charts.Series<transactions, num>> series = [
       charts.Series(
---
+++ coin/lib/transactions_data.dart
@@ -0,0 +1,116 @@
+import 'package:coin/transactions.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+
+class TransactionsData {
+  static final List<transactions> getData = [
+    transactions(
+      date: 1,
+      money: 45,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 2,
+      money: 3,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 3,
+      money: 4,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 4,
+      money: 3,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 5,
+      money: 10,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 6,
+      money: 3,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 7,
+      money: 11,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 8,
+      money: 3,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 9,
+      money: 30,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 10,
+      money: 1,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 11,
+      money: 3,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 12,
+      money: 0,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 13,
+      money: 6,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 14,
+      money: 2,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 15,
+      money: 10,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 16,
+      money: 0,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 17,
+      money: 9,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+    transactions(
+      date: 18,
+      money: 4,
+      barColor:
+          charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 0, 0, 0)),
+    ),
+  ];
+}
