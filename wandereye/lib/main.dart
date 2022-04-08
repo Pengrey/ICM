9ed47c97212bf85d4b6928ae59cc468a6520f321
Pengrey <rodrigoflima@ua.pt>
2022-04-08 14:18:04 +0100

    geolocation stream and example2

--- wandereye/lib/main.dart
+++ wandereye/lib/main.dart
@@ -424,6 +424,50 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  // Location stuff
+  final StreamController<Position> _locStream = StreamController();
+  late StreamSubscription<Position> locationSubscription;
+  Position? lastPosition;
+
+  void showSnackbar(dynamic a) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(a.toString()),
+    ));
+  }
+
+  startLocation() {
+    final positionStream =
+        Geolocator.getPositionStream().handleError((error) {});
+    locationSubscription = positionStream.listen((Position position) {
+      _locStream.sink.add(position);
+
+      // Show first position
+      lastPosition ??= position;
+
+      // Show position if changed
+      if (lastPosition?.latitude != position.latitude ||
+          lastPosition?.longitude != position.longitude) {
+        lastPosition = position;
+        showSnackbar(
+            "Position changed to: ${position.toString()}"); // TODO comment this to stop snackbar spam
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    GeolocatorPlatform.instance.requestPermission();
+    startLocation();
+  }
+
+  @override
+  void dispose() {
+    _locStream.close();
+    locationSubscription.cancel();
+    super.dispose();
+  }
+
   int _bottomNavIndex = 0;
   List<IconData> bottom_navbarIcons = [
     Icons.home_rounded,
@@ -432,19 +476,6 @@ class _MyHomePageState extends State<MyHomePage> {
     Icons.emoji_events_rounded,
     Icons.settings_rounded,
   ];
-
-  static const String _kLocationServicesDisabledMessage =
-      'Location services are disabled.';
-  static const String _kPermissionDeniedMessage = 'Permission denied.';
-  static const String _kPermissionDeniedForeverMessage =
-      'Permission denied forever.';
-  static const String _kPermissionGrantedMessage = 'Permission granted.';
-
-  final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
-  final List<_PositionItem> _positionItems = <_PositionItem>[];
-  StreamSubscription<Position>? _positionStreamSubscription;
-  StreamSubscription<ServiceStatus>? _serviceStatusStreamSubscription;
-  bool positionStreamStarted = false;
 
   List<Widget> youGotMail = <Widget>[
     const SizedBox(height: 70),
@@ -480,180 +511,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ]),
     )
   ];
-
-  Future<void> _getCurrentPosition() async {
-    final hasPermission = await _handlePermission();
-
-    if (!hasPermission) {
-      return;
-    }
-
-    final position = await _geolocatorPlatform.getCurrentPosition();
-    _updatePositionList(
-      _PositionItemType.position,
-      position.toString(),
-    );
-  }
-
-  Future<bool> _handlePermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await _geolocatorPlatform.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      _updatePositionList(
-        _PositionItemType.log,
-        _kLocationServicesDisabledMessage,
-      );
-
-      return false;
-    }
-
-    permission = await _geolocatorPlatform.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await _geolocatorPlatform.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        _updatePositionList(
-          _PositionItemType.log,
-          _kPermissionDeniedMessage,
-        );
-
-        return false;
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      _updatePositionList(
-        _PositionItemType.log,
-        _kPermissionDeniedForeverMessage,
-      );
-
-      return false;
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    _updatePositionList(
-      _PositionItemType.log,
-      _kPermissionGrantedMessage,
-    );
-    return true;
-  }
-
-  void _updatePositionList(_PositionItemType type, String displayValue) {
-    _positionItems.add(_PositionItem(type, displayValue));
-    setState(() {});
-    print(_PositionItem(type, displayValue));
-  }
-
-  void _toggleServiceStatusStream() {
-    if (_serviceStatusStreamSubscription == null) {
-      final serviceStatusStream = _geolocatorPlatform.getServiceStatusStream();
-      _serviceStatusStreamSubscription =
-          serviceStatusStream.handleError((error) {
-        _serviceStatusStreamSubscription?.cancel();
-        _serviceStatusStreamSubscription = null;
-      }).listen((serviceStatus) {
-        String serviceStatusValue;
-        if (serviceStatus == ServiceStatus.enabled) {
-          if (positionStreamStarted) {
-            _toggleListening();
-          }
-          serviceStatusValue = 'enabled';
-        } else {
-          if (_positionStreamSubscription != null) {
-            setState(() {
-              _positionStreamSubscription?.cancel();
-              _positionStreamSubscription = null;
-              _updatePositionList(
-                  _PositionItemType.log, 'Position Stream has been canceled');
-            });
-          }
-          serviceStatusValue = 'disabled';
-        }
-        _updatePositionList(
-          _PositionItemType.log,
-          'Location service has been $serviceStatusValue',
-        );
-      });
-    }
-  }
-
-  void _toggleListening() {
-    if (_positionStreamSubscription == null) {
-      final positionStream = _geolocatorPlatform.getPositionStream();
-      _positionStreamSubscription = positionStream.handleError((error) {
-        _positionStreamSubscription?.cancel();
-        _positionStreamSubscription = null;
-      }).listen((position) => _updatePositionList(
-            _PositionItemType.position,
-            position.toString(),
-          ));
-      _positionStreamSubscription?.pause();
-    }
-
-    setState(() {
-      if (_positionStreamSubscription == null) {
-        return;
-      }
-
-      String statusDisplayValue;
-      if (_positionStreamSubscription!.isPaused) {
-        _positionStreamSubscription!.resume();
-        statusDisplayValue = 'resumed';
-      } else {
-        _positionStreamSubscription!.pause();
-        statusDisplayValue = 'paused';
-      }
-
-      _updatePositionList(
-        _PositionItemType.log,
-        'Listening for position updates $statusDisplayValue',
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    if (_positionStreamSubscription != null) {
-      _positionStreamSubscription!.cancel();
-      _positionStreamSubscription = null;
-    }
-
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    positionStreamStarted = !positionStreamStarted;
-    _toggleServiceStatusStream();
-  }
-
-  void _getLastKnownPosition() async {
-    final position = await _geolocatorPlatform.getLastKnownPosition();
-    if (position != null) {
-      _updatePositionList(
-        _PositionItemType.position,
-        position.toString(),
-      );
-    } else {
-      _updatePositionList(
-        _PositionItemType.log,
-        'No last known position available',
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -916,17 +773,7 @@ class _MyBodyState extends State<Body> {
   }
 }
 
-enum _PositionItemType {
-  log,
-  position,
-}
-
-class _PositionItem {
-  _PositionItem(this.type, this.displayValue);
-
-  final _PositionItemType type;
-  final String displayValue;
-}
+// DEBUG stuff to clean later
 
 class Debug extends StatefulWidget {
   const Debug({Key? key}) : super(key: key);
