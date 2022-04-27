a448eb5cdd264be536cfba6b5e05cc265af526d7
Pengrey <rodrigoflima.me@gmail.com>
2022-04-27 17:20:58 +0100

    Added leaderboard example

---
+++ leaderboard_example/lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:leaderboard_example/users_scores.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: LeaderboardPage(),
+    );
+  }
+}
+
+class LeaderboardPage extends StatelessWidget {
+  LeaderboardPage({Key? key}) : super(key: key);
+
+  List<Map<String, Object>> data = UsersScore.getScores;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const Image(image: AssetImage('assets/podium.png')),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          height: 70,
+                          width: double.maxFinite,
+                          child: Card(
+                            elevation: 5,
+                            child: Padding(
+                              padding: const EdgeInsets.all(7),
+                              child: Stack(children: <Widget>[
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Stack(
+                                    children: <Widget>[
+                                      Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10, top: 5),
+                                          child: Column(
+                                            children: <Widget>[
+                                              Row(
+                                                children: <Widget>[
+                                                  userPosition(
+                                                      data[index]),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  userName(data[index]),
+                                                  const SizedBox(
+                                                    width: 10,
+                                                  ),
+                                                  const Spacer(),
+                                                  userScore(data[index]),
+                                                  const SizedBox(
+                                                    width: 20,
+                                                  )
+                                                ],
+                                              ),
+                                            ],
+                                          ))
+                                    ],
+                                  ),
+                                )
+                              ]),
+                            ),
+                          ),
+                        );
+                      }))
+            ],
+          ),
+        ));
+  }
+
+  static Widget userName(data) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: RichText(
+        text: TextSpan(
+          text: '   ${data['name']}',
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  static Widget userScore(data) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: RichText(
+        text: TextSpan(
+          text: '${data['points']}',
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  static Widget userPosition(data) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: RichText(
+        text: TextSpan(
+          text: '${data['position']}',
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
---
+++ leaderboard_example/lib/users_scores.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class UsersScore {
+  static final getScores = [
+    {
+      'name': 'Rodrigo',
+      'position': '1',
+      'icon': Icons.account_circle,
+      'points': '523',
+    },
+    {
+      'name': 'Camila',
+      'position': '2',
+      'icon': Icons.account_circle,
+      'points': '123',
+    },
+    {
+      'name': 'Diogo',
+      'position': '3',
+      'icon': Icons.account_circle,
+      'points': '32',
+    },
+        {
+      'name': 'Dinis',
+      'position': '4',
+      'icon': Icons.account_circle,
+      'points': '23',
+    },
+    {
+      'name': 'Someone',
+      'position': '5',
+      'icon': Icons.account_circle,
+      'points': '12',
+    },
+  ];
+}
