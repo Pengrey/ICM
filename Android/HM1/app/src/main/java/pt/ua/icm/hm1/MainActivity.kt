abc4eb6d90864ccbb206f43e9c3edc1b34437c8c
Pengrey <rodrigoflima.me@gmail.com>
2022-05-11 17:28:14 +0100

    Only lacks call -> dialer

--- Android/HM1/app/src/main/java/pt/ua/icm/hm1/MainActivity.kt
+++ Android/HM1/app/src/main/java/pt/ua/icm/hm1/MainActivity.kt
@@ -2,8 +2,10 @@ package pt.ua.icm.hm1
 
 import android.app.Dialog
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
@@ -57,6 +59,9 @@ class MainActivity : AppCompatActivity() {
         // Number view
         val numberViewCurrentNumber: Button = findViewById(R.id.numberView)
 
+        // Shared Preferences
+        val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
         // Char buttons
         button0.setOnClickListener {
             onCharButtonClick(zero, numberViewCurrentNumber)
@@ -107,26 +112,59 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Speed dial buttons
+        // Short
+        buttons0.setOnClickListener{
+            onShortSpeedButtonClick(numberViewCurrentNumber, "0", sharedPreferences)
+        }
+
+        buttons1.setOnClickListener{
+            onShortSpeedButtonClick(numberViewCurrentNumber, "1", sharedPreferences)
+        }
+
+        buttons2.setOnClickListener{
+            onShortSpeedButtonClick(numberViewCurrentNumber, "2", sharedPreferences)
+        }
+
+        // Long
         buttons0.setOnLongClickListener{
-            onLongSpeedButtonClick(buttons0)
+            onLongSpeedButtonClick(buttons0, "0", sharedPreferences)
             true
         }
 
         buttons1.setOnLongClickListener{
-            onLongSpeedButtonClick(buttons1)
+            onLongSpeedButtonClick(buttons1, "1", sharedPreferences)
             true
         }
 
         buttons2.setOnLongClickListener{
-            onLongSpeedButtonClick(buttons2)
+            onLongSpeedButtonClick(buttons2, "2", sharedPreferences)
             true
         }
 
         // Action buttons
+        // Delete short press
         buttonDelete.setOnClickListener {
             onDelButtonClick(numberViewCurrentNumber)
         }
 
+        // Delete long press
+        buttonDelete.setOnLongClickListener{
+            onLongDelButtonClick(numberViewCurrentNumber)
+            true
+        }
+
+        // Shared preferences assignments
+        // Speed Dial 0
+        buttons0.text = sharedPreferences.getString(getString(R.string.speed0Nm), "-").toString()
+        val sD0Phone: String = sharedPreferences.getString(getString(R.string.speed0Ph), "").toString()
+
+        // Speed Dial 1
+        buttons1.text = sharedPreferences.getString(getString(R.string.speed1Nm), "-").toString()
+        val sD1Phone: String = sharedPreferences.getString(getString(R.string.speed1Ph), "").toString()
+
+        // Speed Dial 2
+        buttons2.text = sharedPreferences.getString(getString(R.string.speed2Nm), "-").toString()
+        val sD2Phone: String = sharedPreferences.getString(getString(R.string.speed2Ph), "").toString()
     }
     // On char button click func
     private fun onCharButtonClick(char: String, numberViewCurrentNumber: Button) {
@@ -144,7 +182,7 @@ class MainActivity : AppCompatActivity() {
         numberViewCurrentNumber.text = currentValue
     }
 
-    // On delete button click func
+    // On delete short button click func
     private fun onDelButtonClick(numberViewCurrentNumber: Button) {
 
         var currentValue: String = numberViewCurrentNumber.text.toString()
@@ -157,10 +195,13 @@ class MainActivity : AppCompatActivity() {
         numberViewCurrentNumber.text = currentValue
     }
 
-    // On long speed button click func
-    private fun onLongSpeedButtonClick(speedButton: Button) {
-        // TODO persistence
+    // On delete long button click func
+    private fun onLongDelButtonClick(numberViewCurrentNumber: Button) {
+        numberViewCurrentNumber.text = ""
+    }
 
+    // On long speed button click func
+    private fun onLongSpeedButtonClick(speedButton: Button, idx: String, prefs: SharedPreferences) {
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_add_sd)
         val userName = dialog.findViewById(R.id.username) as EditText
@@ -168,16 +209,38 @@ class MainActivity : AppCompatActivity() {
         val saveBtn = dialog.findViewById(R.id.saveBtn) as Button
         val cancelBtn = dialog.findViewById(R.id.canBtn) as Button
         saveBtn.setOnClickListener {
+            // Define Contact Name
             val names = userName.text.toString().split(" ")
-            if(names.size >= 2){
-                speedButton.text = StringBuilder().append(names[0][0]).append(".").append(names[1][0])
+            val newName: String = if(names.size >= 2){
+                StringBuilder().append(names[0][0]).append(".").append(names[1][0]).toString()
             }else if (names.size == 1 && userName.text.toString().isNotBlank()){
-                speedButton.text = userName.text.toString()[0].toString()
+                userName.text.toString()[0].toString()
+            }else{
+                speedButton.text.toString()
             }
+
+            // Define Contact Number
+            val newNum: String = phoneNumber.text.toString().ifEmpty {
+                ""
+            }
+
+            // Set on display
+            speedButton.text = newName
+
+            // Set on preferences
+            val editor = prefs.edit()
+            editor.putString("Nm$idx", newName)
+            editor.putString("Ph$idx", newNum)
+            editor.apply()
+
             dialog.dismiss()
         }
         cancelBtn.setOnClickListener { dialog.dismiss() }
         dialog.show()
+    }
 
+    // On short speed button click func
+    private fun onShortSpeedButtonClick(dispButton: Button, idx: String, prefs: SharedPreferences) {
+        dispButton.text = prefs.getString("Ph$idx", "").toString()
     }
 }
