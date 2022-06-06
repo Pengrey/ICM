49085c7d4102bc0f6ab081cb7e68621a0bcc2829
Pengrey <rodrigoflima.me@gmail.com>
2022-06-06 13:22:47 +0100

    Pseudo register done and redirects to home page also done

--- Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/RegisterPage.kt
+++ Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/RegisterPage.kt
@@ -2,56 +2,37 @@ package pt.ua.icm.icmtqsproject
 
 import android.content.Intent
 import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RegisterPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_page)
 
         // Shared Preferences
         val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-        // Check if logged in already or not
-        val riderId: String? = sharedPreferences.getString("riderId", "")
-        if (riderId != "") {
-            val intent = Intent(this, HomePage::class.java)
-            startActivity(intent)
-        }
-
         // Text Fields
         val emailField: EditText = findViewById(R.id.editEmailAddress)
+        val cidField: EditText = findViewById(R.id.editCID)
         val passwordField: EditText = findViewById(R.id.editPassword)
 
         // Action buttons
-        val signinButton: Button = findViewById(R.id.SignInButton)
-        val registerButton: TextView = findViewById(R.id.registerText)
+        val registerButton: Button = findViewById(R.id.RegisterButton)
 
         // Buttons listeners
-        signinButton.setOnClickListener {
-            signin(emailField, passwordField, sharedPreferences)
-        }
-
         registerButton.setOnClickListener {
-            register()
+            register(emailField, cidField, passwordField, sharedPreferences)
         }
-
     }
 
-    private fun register() {
-        // Got to register page
-        val intent = Intent(this, RegisterPage::class.java)
-        startActivity(intent)
-    }
-
-    private fun signin(emailField: EditText, passwordField: EditText, prefs: SharedPreferences) {
+    private fun register(emailField: EditText, passwordField: EditText, cidField: EditText, prefs: SharedPreferences) {
         val email: String = emailField.text.toString()
+        val cid: String = cidField.text.toString()
         val password: String = passwordField.text.toString()
 
         // Call login api endpoint
