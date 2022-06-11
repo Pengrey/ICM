c4c9777f669de6cfcb1feba51619e5d8959ee87f
Pengrey <rodrigoflima.me@gmail.com>
2022-06-11 15:22:33 +0100

    Asks for location permition only

--- Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/HomePage.kt
+++ Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/HomePage.kt
@@ -1,17 +1,63 @@
 package pt.ua.icm.icmtqsproject
 
+import android.Manifest
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.preference.PreferenceManager
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.*
 import pt.ua.icm.icmtqsproject.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding :  ActivityHomePageBinding=
             DataBindingUtil.setContentView(this, R.layout.activity_home_page)
+
+        // Location
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                0
+            )
+            return
+        }
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for (location in locationResult.locations) {
+                        println("Location: $location")
+                        binding.locationText = location.toString()
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
 
         // Shared Preferences
         val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
@@ -25,3 +71,4 @@ class HomePage : AppCompatActivity() {
         }
     }
 }
+
