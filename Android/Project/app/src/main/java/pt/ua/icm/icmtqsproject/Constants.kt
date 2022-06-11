c49033fad5d12318bb9251cbddfcefcab409feb7
Pengrey <rodrigoflima.me@gmail.com>
2022-06-11 16:34:41 +0100

    Added background location

---
+++ Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/Constants.kt
@@ -0,0 +1,7 @@
+package pt.ua.icm.icmtqsproject
+
+object Constants {
+    val LOCATION_SERVICE_ID = 175
+    val ACTION_START_LOCATION_SERVICE = "startLocationService"
+    val ACTION_STOP_LOCATION_SERVICE = "stopLocationService"
+}
--- Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/HomePage.kt
+++ Android/Project/app/src/main/java/pt/ua/icm/icmtqsproject/HomePage.kt
@@ -1,6 +1,9 @@
 package pt.ua.icm.icmtqsproject
 
 import android.Manifest
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
@@ -8,56 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.location.*
 import pt.ua.icm.icmtqsproject.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding :  ActivityHomePageBinding=
             DataBindingUtil.setContentView(this, R.layout.activity_home_page)
 
         // Location
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                0
-            )
-            return
-        }
-
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
-            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult) {
-                    super.onLocationResult(locationResult)
-                    for (location in locationResult.locations) {
-                        println("Location: $location")
-                        binding.locationText = location.toString()
-                    }
-                }
-            },
-            Looper.myLooper()
-        )
+        initView()
 
         // Shared Preferences
         val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
@@ -69,6 +38,92 @@ class HomePage : AppCompatActivity() {
         }else{
             binding.welcomingText = "No Rider!"
         }
+
+        // Bind Location to text on page
+        val lastLocation: String? = sharedPreferences.getString("lastLocation", "")
+        if (lastLocation != "") {
+            binding.locationText = "$lastLocation"
+        }else{
+            binding.locationText = "No Location Registered!"
+        }
+    }
+
+    private fun initView() {
+        // Delivery State Button buttons
+        val deliveryStateButton: Button = findViewById(R.id.deliveryStateButton)
+        
+        deliveryStateButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_CODE_LOCATION_PERMISSION
+                )
+            } else {
+                if (isLocationServiceRunning()) {
+                    stopLocationService()
+                    println("STOP")
+                } else {
+                    startLocationService()
+                    println("START")
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isLocationServiceRunning(): Boolean {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        activityManager.getRunningServices(Integer.MAX_VALUE).forEach {
+            if (LocationService::class.java.name == it.service.className) {
+                if (it.foreground) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun startLocationService() {
+        if (!isLocationServiceRunning()) {
+            val intent = Intent(applicationContext, LocationService::class.java)
+            intent.action =
+                Constants.ACTION_START_LOCATION_SERVICE
+            startService(intent)
+        }
+    }
+
+    private fun stopLocationService() {
+        if (isLocationServiceRunning()) {
+            val intent = Intent(applicationContext, LocationService::class.java)
+            intent.action =
+                Constants.ACTION_STOP_LOCATION_SERVICE
+            startService(intent)
+        }
+    }
+
+    companion object {
+        private val REQUEST_CODE_LOCATION_PERMISSION = 1
     }
 }
 
